bd6e3edd95dd9bb0f1cd705550259496fe738eb7
martabaker <161087321+martabaker@users.noreply.github.com>
2024-05-01 16:49:01 -0400

    added more code

--- Starter Code/PyBank/main.py
+++ Starter Code/PyBank/main.py
@@ -11,14 +11,26 @@ csvpath = "PyBank/Resources/budget_data.csv"
 
 # Open the CSV file
 with open(csvpath, encoding='UTF-8') as csvfile:
-
+    
     # add in the delimiter and variables that hold the contents of the file
-    csv_reader = csv.reader(csvfile, delimiter=',')
+    csvreader = csv.reader(csvfile, delimiter=",")
+
 
     # Read the header row first
-    csv_header = next(csv_reader)
+    csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
     # DEBUGGING - Check to make sure it's reading the file correctly
-    for row in csv_reader:
-        print(csv_reader)
+    for row in csvreader:
+        month = row[0]
+        profit_loss = row[1]
+        print(f"in {month} the profit was {profit_loss}")
+
+        # Total months; added 1 to the sum to account for the first row being 0
+        # Source: https://stackoverflow.com/questions/16108526/how-to-obtain-the-total-numbers-of-rows-from-a-csv-file-in-python
+        total_months = (sum(1 for row in csvreader) + 1)
+        print(total_months)
+
+        # Total Profit-Losses
+        total = sum(int(profit_loss))
+        print(total)
