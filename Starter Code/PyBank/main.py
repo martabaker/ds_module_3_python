579eeb99efb8172c57ce48c3868b6fa179b90492
martabaker <161087321+martabaker@users.noreply.github.com>
2024-05-03 16:18:20 -0400

    some more framework

--- Starter Code/PyBank/main.py
+++ Starter Code/PyBank/main.py
@@ -7,43 +7,12 @@ import csv
 # Set path for files using relative path with slashes flipped
 csvpath = "PyBank/Resources/budget_data.csv"
 
-# Set variables and create equations to solve the problems
-# def analysis (budget):
-#     month = str(budget[0]) # making sure to know this is a string -- MIGHT NOT NEED THIS
-#     profit_losses = int(budget[1]) # casting this to make sure math can be done with this
 
-
-# # Count the number of lines -- this might need to be solved within the open file
-#     total_months = sum(1 for budget in open('PyBank/Resources/budget_data.csv')) - 1
-
-# # Sum up the Profit/Losses
-#     total_profit = sum(2 for budget in open('PyBank/Resources/budget_data.csv'))
-    
-
-
-
-# # # Find the Greatest Increase in profits and corresponding month
-# #     greatest_increase = #type code here
-
-# # # Find the Greatest Decrease in profits and corresponding month
-# #     greatest_decrease = #type code here
-
-# # Print out the desired results
-#     print("Financial Analysis")
-#     print("------------------------")
-#     print(f"Total Months: {total_months}")
-#     print(f"Total: ${total_profit}")
-#     print(f"Average Change: ${average_change}")
-#     # print(f"Greatest Increase in Profits: {month} (${greatest_increase})")
-#     # print(f"Greatest Decrease in Profits: {month} (${greatest_decrease})")
-
-
-
-#defining total_profit
+#defining total_profit and lists
 total_profit = 0 #will hold the sum
 profit = [] # this list will hold the profits for each month
 index = 0
-mtm_change = [] # this list will hold the month to month changes in profits
+mtm_change = {} # this dictionary will hold the month to month changes in profits and the corresponding month where they occur
 
 # Open the CSV file
 with open(csvpath, encoding='UTF-8') as csvfile:
@@ -87,11 +56,21 @@ with open(csvpath, encoding='UTF-8') as csvfile:
         
         
     # print values
-    print(total_months)
-    print(total_profit)
-    print(positive)
-    print(negative)
-    print(average_change)
-    print(greatest_increase)
-    print(profit)
-    print(len(profit))
+    # print(total_months)
+    # print(total_profit)
+    # print(positive)
+    # print(negative)
+    # print(average_change)
+    # print(greatest_increase)
+    # print(profit)
+    # print(len(profit))
+
+
+# # Print out the desired results
+    # print("Financial Analysis")
+    # print("------------------------")
+    # print(f"Total Months: {total_months}")
+    # print(f"Total: ${total_profit}")
+    # print(f"Average Change: ${average_change}")
+    # print(f"Greatest Increase in Profits: {month} (${greatest_increase})")
+    # print(f"Greatest Decrease in Profits: {month} (${greatest_decrease})")
